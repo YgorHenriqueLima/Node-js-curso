bb8d2a71d83cd133483adbd472274aeeb1053757
YgorHenriqueLima <118612433+YgorHenriqueLima@users.noreply.github.com>
2025-01-31 16:00:31 -0300

    .

--- aula08/db.js
+++ aula08/db.js
@@ -15,12 +15,34 @@ const conectar = async () => {
     return conexao;
 };
 
-const todosClientes = async () => {
+const insereCliente = async (cliente) => {
     const con = await conectar();
-    const [linhas] = await con.query("SELECT * FROM cliente_node"); // Ajuste de nome de tabela, se necessário.
-    return linhas;
+    const sql = 'INSERT INTO cliente_node (nome, idade) VALUES(?,?)';
+    const valores = [cliente.nome, cliente.idade];
+    await con.query(sql,valores);    
 };
 
+
+const todosClientes = async (cliente) => {
+    const con = await conectar();
+    const [linhas] = await con.query("SELECT * FROM cliente_node"); // Ajuste de nome de tabela, se necessário.
+    return await linhas;
+};
+
+const atualizaCliente = async (id, cliente) => {
+    const con = await conectar();
+    const sql = 'UPDATE cliente_node SET nome=?, idade=? WHERE id=?';
+    const valores = [cliente.nome, cliente.idade,id];
+    await con.query(sql,valores)
+};
+
+const deletarCLiente = async (id) =>{
+    const con = await conectar();
+    const sql = 'DELETE FROM cliente_node WHERE id=?';
+    const valores = [ id ];
+    await con.query(sql,valores)
+}
+ 
 // Exporte a função após a definição
-module.exports = { todosClientes };
+module.exports = { todosClientes, insereCliente, atualizaCliente, deletarCLiente };
 
--- aula08/index.js
+++ aula08/index.js
@@ -1,5 +1,23 @@
 (async () => {
     const db = require("./db");
+    
+    /*
+    console.log("novo cliente inserido");
+    let n = "Gustavo Pinheiro";
+    let i = 26;
+    await db.insereCliente({nome: n, idade: i});
+    */
+
+    /*
+    await db.atualizaCliente(4, {nome:"Gabrielson",idade:32});
+    console.log(`cliente atualizado`)
+    */
+   
+    const id = 2
+    await db.deletarCLiente(id)
+    console.log('cliente ' + id + 'deletado')
+
+    /*chamada para consulta dos dados */
     console.log("obter todos os clientes");
     const clientes = await db.todosClientes();
     console.log(clientes);
